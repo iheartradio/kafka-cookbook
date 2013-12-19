2bb2ed787a4f29e7a711a5208724dde4a2dfce4d
Pavel Katsev <pkatsev@gmail.com>
2013-12-19 14:34:39 -0500

    ruby1.9 syntax fixes

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -9,8 +9,8 @@ template File.join(node[:kafka][:config_dir], node[:kafka][:log4j_config]) do
   group node[:kafka][:group]
   mode  '644'
   variables(
-    process: 'kafka',
-    log_dir: node[:kafka][:log_dir]
+    :process => 'kafka',
+    :log_dir => node[:kafka][:log_dir]
   )
 end
 
@@ -27,15 +27,19 @@ template '/etc/init.d/kafka' do
   group 'root'
   mode '755'
   variables(
-    daemon_name:  'kafka',
-    main_class:   'kafka.Kafka',
-    jmx_port:     node[:kafka][:jmx_port],
-    log4j_config: node[:kafka][:log4j_config],
-    config:       node[:kafka][:config]
+    :daemon_name => 'kafka',
+    :main_class => 'kafka.Kafka',
+    :jmx_port => node[:kafka][:jmx_port],
+    :log4j_config => node[:kafka][:log4j_config],
+    :config => node[:kafka][:config]
   )
 end
 
 service 'kafka' do
-  supports start: true, stop: true, restart: true
+  supports(
+      :start => true,
+      :stop => true,
+      :restart => true
+  )
   action [:enable]
 end
